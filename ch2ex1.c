de8534e6a4ee62266748361fe47f7bc3dd2523cc
Dustin Peterson <dpeterson@unknown001D604C46A1.attlocal.net>
2018-10-02 15:26:23 -0500

    KNR C 2-1 Excercise

---
+++ ch2ex1.c
@@ -0,0 +1,24 @@
+//Chapter 2 Excercise 1 from KnR C
+
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main()
+{
+  // char char_min = CHAR_MIN;
+  // char char_max = CHAR_MAX;
+  // char unsigned_char_max = UCHAR_MAX;
+  // int int_min = INT_MIN;
+  // int int_max = INT_MAX;
+  // int unsigned_int_min = UINT_MIN;
+  // int unsigned_int_max = UINT_MAX;
+  // int short_min = SHRT_MIN;
+  // int short_max = SHRT_MAX;
+  printf("The minimum value of a signed Char is: %c\n", CHAR_MIN);
+  printf("The minimum value of a signed Int is: %d\n", INT_MIN);
+
+  return 0;
+}
+  
+  
